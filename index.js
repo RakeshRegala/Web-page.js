13654652ee16a4aba0cfbc71a3550b4ff97f1bfd
RakeshRegala <rakeshregala3@example.com>
2025-03-01 23:48:16 +0530

    Added express

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Hi");
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/home', (req, res) => {
+    res.send('I am home page,hello');
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
